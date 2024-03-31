c8e1d2b319b18b760e40278eb2e540818e46ddbb
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-31 16:25:13 -0700

    environmental variable config

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/authRoute.js"
+import { connectDB } from "./config/db.js"
+import cors from "cors"
 const app=express()
-
 dotenv.config()
+const PORT=process.env.PORT ||3000
+app.use(express.json())
+app.use("/api/auth",authRoutes)
 app.get("/",(req,res)=>{
     res.send('hey how are u')
 })
-app.use("/api/auth",authRoutes)
-const PORT=process.env.PORT ||3000
+// app.use(cors)
+
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`server is running on ${PORT}`)
 })
