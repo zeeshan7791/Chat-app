5dc93444d0185d5e1b0aa21cb6dfa44e8583ffd1
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-30 23:49:07 -0700

    sign up route created

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
+import { login, logout, signUp } from "../controllers/authController.js"
 const router=express.Router()
 
-router.get("/login",(req,res)=>{
-    res.send('hey login')
-})
+router.post("/sign-up",signUp)
+router.get("/login",login)
+router.get("/logout",logout)
 export default router
