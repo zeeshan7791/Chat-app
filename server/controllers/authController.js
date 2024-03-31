6053b473e05a0903c4182f10fb52bfcee50d0822
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-03-30 23:48:39 -0700

    sign up api created and password hashed

---
+++ server/controllers/authController.js
@@ -0,0 +1,74 @@
+import User from "../models/userModel.js"
+import bcryptjs from "bcryptjs";
+export const signUp = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
+console.log(req.body)
+
+		if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords don't match" });
+		}
+
+		const user = await User.findOne({ username });
+
+		if (user) {
+			return res.status(400).json({ error: "Username already exists" });
+		}
+
+		// // HASH PASSWORD HERE
+		const salt = await bcryptjs.genSalt(10);
+		const hashedPassword = await bcryptjs.hash(password, salt);
+
+		// // https://avatar-placeholder.iran.liara.run/
+
+		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+		const newUser = new User({
+			fullName,
+			username,
+			password:hashedPassword,
+			gender,
+			profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+		});
+
+		// if (newUser) {
+		// 	// Generate JWT token here
+		// 	generateTokenAndSetCookie(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				profilePic: newUser.profilePic,
+			});
+		// } else {
+		// 	res.status(400).json({ error: "Invalid user data" });
+		// }
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
+export const login=async(req,res,next)=>{
+    try{
+        res.send("login")
+
+console.log(req.body)
+    }
+    catch(error){
+        next(error)
+    }
+}
+export const logout=async(req,res,next)=>{
+    try{
+        res.send("logout")
+
+console.log(req.body)
+    }
+    catch(error){
+        next(error)
+    }
+}
