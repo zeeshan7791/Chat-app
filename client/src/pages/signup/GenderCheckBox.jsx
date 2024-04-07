3786c989549ccbf91853874550bfcb46e7ec3dc3
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-07 13:22:40 -0700

    sign up page UI completed

---
+++ client/src/pages/signup/GenderCheckBox.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
+	return (
+		<div className='flex'>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
+					<span className='label-text'>Male</span>
+					<input
+						type='checkbox'
+						className='checkbox border-slate-900'
+						checked={selectedGender === "male"}
+						onChange={() => onCheckboxChange("male")}
+					/>
+				</label>
+			</div>
+			<div className='form-control'>
+				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
+					<span className='label-text'>Female</span>
+					<input
+						type='checkbox'
+						className='checkbox border-slate-900'
+						checked={selectedGender === "female"}
+						onChange={() => onCheckboxChange("female")}
+					/>
+				</label>
+			</div>
+		</div>
+	);
+};
+GenderCheckbox.propTypes = {
+	onCheckboxChange: PropTypes.func.isRequired,
+	selectedGender: PropTypes.oneOf(["male", "female"]).isRequired
+  }
+export default GenderCheckbox;
+
