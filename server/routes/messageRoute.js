92eddf3435694e167250fffaa6ee607334aff623
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-07 13:17:09 -0700

    message route completed

--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { sendMessage } from "../controllers/messagesController.js"
+import { getMessages, sendMessage } from "../controllers/messagesController.js"
 import protectRoute from "../middleware/protectedRoute.js"
 
 const router=express.Router()
 
 router.post("/send/:id",protectRoute,sendMessage)
+router.get("/:id",protectRoute,getMessages)
 
 export default router
