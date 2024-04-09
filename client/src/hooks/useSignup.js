901a699400215a60104ee17ebaa58cb6de91b381
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-09 16:36:34 -0700

    use sign up hook function created

---
+++ client/src/hooks/useSignup.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const useSignup = () => {
+  const [loading, setLoading] = useState(false);
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!success) return;
+
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:8000/api/auth/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        toast.error("server error");
+      }
+      if (data.success) {
+        toast.success(data.message);
+
+        localStorage.setItem("chat-user", JSON.stringify(data));
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+};
+export default useSignup;
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("Passwords do not match");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+
+  return true;
+}
