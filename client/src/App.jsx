43db50ada551f3bfd7e9082befd231266a859aed
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-17 21:19:40 -0700

    navigation added in components routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./pages/Home"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin"
 import Signup from "./pages/signup/Signup"
 import  { Toaster } from 'react-hot-toast';
+import { useAuthContext } from "./context/AuthContext";
 function App() {
-
+const {authUser}=useAuthContext()
 
   return (
     <>
@@ -14,9 +15,9 @@ function App() {
    <BrowserRouter>
       <Routes>
         
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={authUser?<Home />:<Navigate to="/login"/>} />
+          <Route path="/login" element={authUser?<Navigate to="/"/>:<Signin />} />
+          <Route path="/signup" element={authUser?<Navigate to="/"/>:<Signup />} />
           
      
       </Routes>
