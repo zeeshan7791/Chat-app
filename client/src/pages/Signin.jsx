334c4059afe497787db0803f55fd0894dc5a667b
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-17 21:15:59 -0700

    sign in functionality completed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 
 const Signin = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
-	
+const{loading,login}	=useLogin()
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		console.log('sign in')
+	await login(username,password)
 	};
-let loading =false
+
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
