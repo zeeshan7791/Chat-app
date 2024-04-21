c95ec366d68b114f597c36409c5e4d076706b163
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-21 11:29:25 -0700

    dynamics values mapped

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,5 +1,5 @@
 
-const Conversation = () => {
+const Conversation = ({conversation,emoji,lastIdx}) => {
 	return (
 		<>
 			<div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
@@ -14,13 +14,13 @@ const Conversation = () => {
 
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap-3 justify-between'>
-						<p className='font-bold text-gray-200'>John Doe</p>
-						<span className='text-xl'>🎃</span>
+						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
+						<span className='text-xl'>{emoji}</span>
 					</div>
 				</div>
 			</div>
 
-			<div className='divider my-0 py-0 h-1' />
+{!lastIdx&&<div className='divider my-0 py-0 h-1' />}
 		</>
 	);
 };
