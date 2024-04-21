45604551737029dd77b8d76162bef3c12a041812
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-21 11:27:47 -0700

    token functionality completed

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,9 +8,13 @@ const token=jwt.sign({userId},process.env.JWT_SECRET,{
 res.cookie("chatToken",token,
 {
     maxAge:15*24*60*60*1000,
-    httpOnly:true, //prevent XXS attackS cross-site scripting attacks
-    sameSite:"strict", //CSRF attacks cross-site request forgery attacks 
-    secure: process.env.NODE_ENV !== "development",
+    // httpOnly:true, //prevent XXS attackS cross-site scripting attacks
+    // sameSite: "None", //CSRF attacks cross-site request forgery attacks 
+    // secure: process.env.NODE_ENV !== "development",
+    httpOnly: true,
+    path: "/",
+    sameSite: "None", // Only use with HTTPS
+    secure: true, // Only use with HTTPS
 
 })
 }
