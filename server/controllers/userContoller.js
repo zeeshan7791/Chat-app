534769e908d51e169cb8b36b73f4156841a9c052
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-21 11:25:11 -0700

    user functionality completed

--- server/controllers/userContoller.js
+++ server/controllers/userContoller.js
@@ -1,13 +1,15 @@
 
 import User from "../models/userModel.js";
 
-export const getAllUsers = async (req, res) => {
+export const getUsersForSidebar = async (req, res) => {
 	try {
 		const loggedInUserId = req.user._id;
-
+console.log(loggedInUserId,'value in logined')
 		const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-		res.status(200).json(filteredUsers);
+		return res.status(200).json({success:true,
+			message:"user fetched successfully",
+			filteredUsers});
 	} catch (error) {
 		console.error("Error in getUsersForSidebar: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
