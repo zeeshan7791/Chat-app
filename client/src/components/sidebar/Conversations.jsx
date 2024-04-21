fd441150e2c370d2404f8120ad47478fd92791b7
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-21 11:30:07 -0700

    consersations fetched using getconversation hook

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,17 +1,24 @@
-
-
 import Conversation from "./Conversation";
-
+import useGetConversations from "../../hooks/useGetConversations.js";
+import { getRandomEmoji } from "../../utils/emojis.js";
 const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+	
 	return (
+		<>
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
 		</div>
+		</>
 	);
 };
 export default Conversations;
