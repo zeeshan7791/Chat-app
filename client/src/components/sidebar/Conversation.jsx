ac9a5e9563508e8840f3e30b4ea683ecb63bdd30
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-26 11:14:45 -0700

    side conversation user list functionality completed

--- client/src/components/sidebar/Conversation.jsx
+++ client/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,13 @@
+import useConversation from "../../zustand/useConverstation";
 
 const Conversation = ({conversation,emoji,lastIdx}) => {
+	const{selectedConversation,setSelectedConversation}=useConversation()
+	const isSelected=selectedConversation?._id===conversation._id
 	return (
 		<>
-			<div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+			<div className={`flex gap-2 items-centerrounded p-2 py-1 cursor-pointer ${isSelected? "bg-sky-500":"" }`}
+			onClick={() => setSelectedConversation(conversation)}
+			>
 				<div className='avatar online'>
 					<div className='w-12 rounded-full'>
 						<img
