f18652246b76046b403e63f9584f5fcf88bc161a
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-04-26 11:20:13 -0700

    cors functionality completed

--- server/index.js
+++ server/index.js
@@ -4,13 +4,22 @@ import authRoutes from "./routes/authRoute.js"
 import messagesRoute from "./routes/messageRoute.js"
 import userRoute from "./routes/userRoute.js"
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 import { connectDB } from "./config/db.js"
 import cors from "cors"
 const app=express()
 dotenv.config()
 const PORT=process.env.PORT ||3000
-app.use(cors())
-app.use(express.json())
+
+// app.use(cors())
+app.use(
+    cors({
+      origin: "http://127.0.0.1:3000",
+      credentials: true, // Allow credentials (cookies)
+    })
+  );
+  app.use(express.json())
+  app.use(morgan("dev"));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
