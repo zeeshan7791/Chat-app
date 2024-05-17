1e08d1372c69f268c2a14d4084a3a971357fd8eb
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-05-17 10:16:09 -0700

    socket io context created

---
+++ client/src/context/SocketContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState, useRef } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
+
+export const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+    const [onlineUsers, setOnlineUsers] = useState([]);
+    const { authUser } = useAuthContext();
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        if (authUser) {
+            const socket = io("http://localhost:8000", {
+                query: {
+                    userId: authUser._id,
+                },
+            });
+            socketRef.current = socket;
+
+            socket.on("getOnlineUsers", (users) => {
+                setOnlineUsers(users);
+            });
+
+            return () => {
+                socket.close();
+                socketRef.current = null;
+            };
+        } else {
+            if (socketRef.current) {
+                socketRef.current.close();
+                socketRef.current = null;
+            }
+        }
+    }, [authUser]);
+
+    return (
+        <SocketContext.Provider value={{ socket: socketRef.current, onlineUsers }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
