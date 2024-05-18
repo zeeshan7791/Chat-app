29c9e7f3561d7076b3b65f14cc699fa228cf58cf
Zeeshan Yousaf <zeeshanento34@gmail.com>
2024-05-18 12:17:05 -0700

    local link added to server link

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,2 +1,2 @@
-// export const serverLink="http://localhost:8000/api"
-export const serverLink="https://chat-app-production-5bbn.onrender.com/api"
+export const serverLink="http://localhost:8000/api"
+// export const serverLink="https://chat-app-production-5bbn.onrender.com/api"
